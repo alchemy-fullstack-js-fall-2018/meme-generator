17ebca93dcb9a8bd306c84eb08e3ca9f7aa5f5f2
Jack <ajackofsometrades@gmail.com>
2018-11-13 15:50:55 -0800

    initial scaffolding
    more scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends Component {
+
+
+  render() {
+
+    return (
+      <Fragment>
+        <h1>TESTTESTSET</h1>
+      </Fragment>
+    );
+  }
+
+
+}
