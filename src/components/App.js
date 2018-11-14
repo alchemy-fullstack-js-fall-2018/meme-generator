b83be1c1001e365c847dd87ffd8af8d645b386a9
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-14 12:28:27 -0800

    meme generator has basic, imperfect functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,59 @@
 import React, { Component, Fragment } from 'react';
+import styles from './App.css';
 export default class App extends Component {
     state = {
-        imgSource: 'URL' 
+        imgSource: 'URL',
+        img: '',
+        topText: '',
+        bottomText: ''
     };
 
+    onChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    };
+
+    onUpload =  ({ target }) => {
+        console.log('files[0]', target.files[0]);
+        let imgURL = window.URL.createObjectURL(target.files[0]);
+        console.log('imgURL', imgURL);
+        this.setState({ img: imgURL });
+    };
+
+    onText = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    }
+
     render() {
-        const { imgSource } = this.state;
-        const options = ['URL', 'Upload'].map(option => {
-            return <option key={option} value={option}>{option}</option>;
+        const { imgSource, img, topText, bottomText } = this.state;
+        const options = ['URL', 'Upload'].map(imgSource => {
+            return <option key={imgSource} value={imgSource}>{imgSource}</option>;
         });
+        let input = <input type="text" name="img" value={img} onChange={this.onChange}/>;
+        if(imgSource === 'Upload') {
+            input = <input type="file" name="img" onChange={this.onUpload}/>;
+        }
+        let topTextInput = <input type="text" name="topText" value={topText} onChange={this.onText}/>;
+        let bottomTextInput = <input type="text" name="bottomText" value={bottomText} onChange={this.onText}/>;
 
         return (
             <Fragment>
                 <h1>Hello Meme Generator</h1>
                 <form>
-                    <select name="imgSource" defaultValue={imgSource}>
+                    <select name="imgSource" defaultValue={imgSource} onChange={this.onChange}>
                         {options}
                     </select>
+                    {input}
+                    {topTextInput}
+                    {bottomTextInput}
                 </form>
+                {img && <div className={styles.card} style={{ backgroundImage: `url(${img})` }}>
+                    <p className={styles.topText}>{topText}</p>
+                    <p className={styles.bottomText}>{bottomText}</p>
+                </div>}
             </Fragment>
         );
     }
 }
+
+// objectURL = URL.createObjectURL(object);
+// target.files[0]
