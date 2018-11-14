1c5f9ffa06c35f9704768777d6f5c343d35a4cd1
Ryan Luras <ryanluras@gmail.com>
2018-11-14 10:25:10 -0800

    dom to image working

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, { Component, Fragment } from 'react';
 import image1 from '../assets/image1.jpg';
 import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
-
-// import domToImage from 'dom-to-image';
+import styles from './App.css';
+import domToImage from 'dom-to-image';
 // import fileSaver from 'file-saver';
-// import styles from './App.css';
+
 const imageMap = [
   {
     title: 'drake',
@@ -25,15 +25,23 @@ export default class App extends Component {
   state = {
     topText: '',
     bottomText: '',
-    selectedImg: image1
+    selectedImg: image1,
+    memeImg: ''
   };
 
   onChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   };
 
+  memeToImage = event => {
+    event.preventDefault();
+    domToImage.toPng(document.getElementById('meme')).then(memeImg => {
+      this.setState({ memeImg });
+    });
+  };
+
   render() {
-    const { topText, bottomText, selectedImg } = this.state;
+    const { topText, bottomText, selectedImg, memeImg } = this.state;
     const options = imageMap.map(img => {
       return (
         <option key={img.title} value={img.src}>
@@ -43,32 +51,38 @@ export default class App extends Component {
     });
 
     return (
-      <Fragment>
-        <form>
-          <select
-            name="selectedImg"
-            defaultValue={selectedImg}
-            onChange={this.onChange}
-          >
-            {options}
-          </select>
-        </form>
-        <input
-          name="topText"
-          value={topText}
-          placeholder="top text"
-          onChange={this.onChange}
-        />
-        <input
-          name="bottomText"
-          value={bottomText}
-          placeholder="bottom text"
-          onChange={this.onChange}
-        />
-        {bottomText}
-        {topText}
-        <img src={selectedImg} />
-      </Fragment>
+      <div className={styles.generator}>
+        <section className={styles.formHolder}>
+          <form onSubmit={this.memeToImage}>
+            <select
+              name="selectedImg"
+              defaultValue={selectedImg}
+              onChange={this.onChange}
+            >
+              {options}
+            </select>
+            <input
+              name="topText"
+              value={topText}
+              placeholder="top text"
+              onChange={this.onChange}
+            />
+            <input
+              name="bottomText"
+              value={bottomText}
+              placeholder="bottom text"
+              onChange={this.onChange}
+            />
+            <button type="submit">Create Image</button>
+          </form>
+        </section>
+        <section id="meme" className={styles.meme}>
+          <div className={styles.topText}>{topText}</div>
+          <img src={selectedImg} />
+          <div className={styles.bottomText}>{bottomText}</div>
+        </section>
+        {memeImg && <img src={memeImg} />}
+      </div>
     );
   }
 }
