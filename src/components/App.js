3cf4f2401e1faeba53391efc57b1f98162d3ce5c
Ryan Luras <ryanluras@gmail.com>
2018-11-13 17:58:37 -0800

    image showing from selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React, { Component, Fragment } from 'react';
+import image1 from '../assets/image1.jpg';
+import image2 from '../assets/image2.jpg';
+import image3 from '../assets/image3.jpg';
+
 // import domToImage from 'dom-to-image';
 // import fileSaver from 'file-saver';
 // import styles from './App.css';
+const imageMap = [
+  {
+    title: 'drake',
+    src: image1
+  },
+  {
+    title: 'austin',
+    src: image2
+  },
+  {
+    title: 'leo',
+    src: image3
+  }
+];
 
 export default class App extends Component {
   state = {
     topText: '',
-    bottomText: ''
+    bottomText: '',
+    selectedImg: image1
   };
 
   onChange = ({ target }) => {
@@ -14,22 +33,42 @@ export default class App extends Component {
   };
 
   render() {
-    const { topText, bottomText } = this.state
-    const options = ['image1', 'image2', 'image3', 'image4', 'image5'].map(img => {
-      return <option key={img} value={img}>{img}</option>;
+    const { topText, bottomText, selectedImg } = this.state;
+    const options = imageMap.map(img => {
+      return (
+        <option key={img.title} value={img.src}>
+          {img.title}
+        </option>
+      );
     });
 
     return (
       <Fragment>
         <form>
-          <select>
+          <select
+            name="selectedImg"
+            defaultValue={selectedImg}
+            onChange={this.onChange}
+          >
             {options}
           </select>
         </form>
-        <input name="topText" value={topText} placeholder="top text" onChange={this.onChange} />
-        <input name="bottomText" value={bottomText} placeholder="bottom text" onChange={this.onChange} />
+        <input
+          name="topText"
+          value={topText}
+          placeholder="top text"
+          onChange={this.onChange}
+        />
+        <input
+          name="bottomText"
+          value={bottomText}
+          placeholder="bottom text"
+          onChange={this.onChange}
+        />
+        {bottomText}
+        {topText}
+        <img src={selectedImg} />
       </Fragment>
     );
-
   }
 }
