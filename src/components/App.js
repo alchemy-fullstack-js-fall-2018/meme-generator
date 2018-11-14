a7124e73ad90ba3cdf5c5d66005c9937013802e9
mlamb <mlamb128@gmail.com>
2018-11-14 11:36:24 -0800

    Progress towards conditionally changing font style

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,100 @@
 import React, { Component, Fragment } from 'react';
 import domToImage from 'dom-to-image';
+
 import fileSaver from 'file-saver';
 import styles from './App.css';
 
-export default class App extends Component{
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   state = {
     header: 'Meme',
     footer: 'Me!',
-    url: 'https://i.ytimg.com/vi/SB-qEYVdvXA/hqdefault.jpg'
+    color: { color: 'Yellow' },
+    font: 'Standard',
+    source:
+      'http://static.origos.hu/s/img/i/1801/20180108doge-dogecoin-kriptovaluta.jpg',
+    sourceType: 'url',
+    file: ''
+  };
+
+  onChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
   };
 
 
-  // displayImage = (event) => {
-  //   event.preventDefault();
-  //   domToImage.toPng(document.getElementById('url'))
-  //     .then(img => {
-  //       this.setState({ img });
-  //     });
-  // }
-  onChange = ({ target }) => {
-    this.setState({ [target.name]: target.value });
+  uploadFile = ({ target }) => {
+    let source = window.URL.createObjectURL(target.files[0]);
+    this.setState({ source });
   }
 
+  memeToImage = (event) => {
+    event.preventDefault();
+    domToImage.toPng(document.getElementById('meme'))
+      .then(file => {
+        fileSaver.saveAs(file);
+      });
+  }
 
 
   render() {
 
-    const { header, footer, url } = this.state;
+    const { header, footer, color, font, source } = this.state;
+    const colorOptions = ['Yellow', 'Red', 'White', 'Black'].map(color => {
+      return <option key={color} value={color}>{color}</option>;
+    });
+    const fontOptions = ['Standard', 'Banner', 'Alligator2', 'Alpha'].map(font => {
+      return <option key={font} value={font}>{font}</option>;
+    });
 
     return (
       <Fragment>
         <h1>Who is ready to Meme?</h1>
-        <form>
-          <div id="urlUploader">
-            <label htmlFor="url">Upload image via URL here</label>
-            <input name="url" id="url" type="url" value={url} onChange={this.onChange}/>
 
-            <label htmlFor="header">Header:</label>
-            <input name="header" id="header" type="header" value={header} onChange={this.onChange}/>
-
-            <label htmlFor="footer">Footer:</label>
-            <input name="footer" id="footer" type="footer" value={footer} onChange={this.onChange}/>
-            <button type="submit">Submit</button>
+        <form onSubmit={this.memeToImage}>
+          <div id='uploader'>
+            <label htmlFor='source'>Type in the image URL here:</label>
+            <input name='source'id='source' type='source' value={source} onChange={this.onChange} />
+            <h3>or</h3>
+            <label htmlFor='file'>Upload image from you computer</label>
+            <input name="file" id="file" type="file" onChange={this.uploadFile}/>
           </div>
 
+          <div id='textInput'>
+            <label htmlFor='header'>Header:</label>
+            <input name='header' id='header' type='header' value={header} onChange={this.onChange} />
+
+            <label htmlFor='footer'>Footer:</label>
+            <input name='footer' id='footer' type='footer' value={footer} onChange={this.onChange} />
+
+            <label htmlFor='color'>Text Color</label>
+            <select name='color' id='color' defaultValue={color} onChange={this.onChange}>
+              {colorOptions}
+            </select>
+
+            <label htmlFor='font'>Text Font</label>
+            <select name="font" defaultValue={font} onChange={this.onChange}>
+              {fontOptions}
+            </select>
+          </div>
+
+          <button type="submit">Save Meme</button>
         </form>
-        <div className={styles.meme} id="meme">
-          <img className={styles.image} src={url} alt="meme image"/>
+
+        <div className={styles.meme} id='meme'>
+          <img className={styles.image} id="image" src={source} alt='meme image' />
           <div className={styles.memeText}>
-            <span className={styles.header} id={header}>{header}</span>
-            <span className={styles.footer} id={footer}>{footer}</span>
+            <span className={styles.header} id={header}>
+              {header}
+            </span>
+            <span className={styles.footer} id={footer}>
+              {footer}
+            </span>
           </div>
         </div>
       </Fragment>
     );
   }
 }
-
-
-       {/* <h4>or</h4>
-          <div id="fileUploader">
-          <label htmlFor="file">Upload image from you computer</label>
-          <input name="file" id="file" type="file"/>
-        </div> */}
