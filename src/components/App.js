8865cb2ec44cfca2bfb6c0324f5116253de5533b
Davidchhing <davidhchhing@gmail.com>
2018-11-14 11:08:25 -0800

    adding colors select to form

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,22 @@ import domToImage from 'dom-to-image';
 import fileSaver from 'file-saver';
 import styles from './App.css';
 import PropTypes from 'prop-types';
+const colorsOptions = require('colors');
 
 export default class App extends Component {
   state = {
     font: 'Standard',
     text: '',
     formatedText: '',
-    img: ''
+    img: '',
+    color: ''
   };
 
   onChange = ({ target }) => {
     // setState in async. It waits for all components to update
     this.setState({ [target.name]: target.value, img: '' }, () => {
-      const { text, font } = this.state;
-      figlet.text(text, { font }, (err, formatedText) => {
+      const { text, font, color, img } = this.state;
+      figlet.text(text, { font, color, img }, (err, formatedText) => {
         this.setState({ formatedText });
       });
     });
@@ -31,15 +33,27 @@ export default class App extends Component {
       });
   };
 
+  // fontToImage = (event) => {
+  //   event.preventDefault();
+  //   domToImage.toPng(document.getElementById('image'))
+  //     .then(img => {
+  //       this.setState({ img });
+  //     });
+  // };
+
   saveImage = () => {
     fileSaver.saveAs(this.state.img);
   };
 
   render() {
-    const { font, text, formatedText, img, url } = this.state;
+    const { font, text, formatedText, img } = this.state;
     const options = ['Standard', 'Bell', '3-D', 'Avatar', 'Barbwire'].map(font => {
       return <option key={font} value={font}>{font}</option>;
     });
+
+    // const colorsOptions = ['Blue', 'Red', 'Green', 'Green', 'Magenta'].map(color => {
+    //   return <option key={color} value={color}>{color}</option>;
+    // });
 
     return (
       <Fragment>
@@ -47,6 +61,12 @@ export default class App extends Component {
           <select name="font" defaultValue={font} onChange={this.onChange}>
             {options}
           </select>
+
+          <select name="color" defaultValue={color} onChange={this.onChange}>
+          {colorsOptions}
+          </select>
+
+
           <input name="text" placeholder="put text here" value={text} onChange={this.onChange} />
           <button type="submit">Create Image</button>
 
@@ -54,10 +74,6 @@ export default class App extends Component {
         <label for="url">Enter image URL or upload image</label>
         <input type="url" name="url" id="url" placeholder="Enter Image URL" value={url} onChange={this.onChange} />
         </div>
-        </form>
-
-
-
         <div className={styles.image}>
           <span id="image" >
             {formatedText}
@@ -65,30 +81,15 @@ export default class App extends Component {
         </div>
         {img && <img src={img} />}
         {img && <button onClick={this.saveImage}>Save Image</button>}
+
+        </form>
+
+
+
+
       </Fragment>
     );
   }
 
 
 }
-
-
-
-
-// import alchemy from '../assets/alchemy.png';
-
-// export default class Header extends Component {
-//   static propTypes = {
-//     title: PropTypes.string
-//   };
-
-//   render() {
-//     return (
-//       <header>
-//         <img src={alchemy} />
-//         <h1>{this.props.title || 'Default Title'}</h1>
-//       </header>
-
-//     );
-//   }
-// }
