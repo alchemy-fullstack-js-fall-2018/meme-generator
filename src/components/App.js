7df907eb0174bfbb4266eb0c9601cf90f1c9513c
mlamb <mlamb128@gmail.com>
2018-11-13 16:44:53 -0800

    Add forms

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,19 @@ export default class App extends Component{
   render() {
     return (
       <Fragment>
-        <h1>Hello World</h1>
+        <h1>Who is ready to Meme?</h1>
+        <form>
+          <div id="urlUploader">
+            <label htmlFor="url">Upload image via URL here</label>
+            <input name="url" id="url" type="url"/>
+            <button>Submit</button>
+          </div>
+          <h4>or</h4>
+          <div id="fileUploader">
+            <label htmlFor="file">Upload image from you computer</label>
+            <input name="file" id="file" type="file"/>
+          </div>
+        </form>
       </Fragment>
     );
   }
