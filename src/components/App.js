07574b691d2af9573bd7d9ca644ecac1a906d48a
KLPainter <KLPainter@gmail.com>
2018-11-14 12:03:10 -0800

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,60 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import domToImage from 'dom-to-image';
 import fileSaver from 'file-saver';
+import styles from './App.css';
 
 export default class App extends Component {
 
   state = {
-    header: '',
-    footer: '',
-    font: '',
-    color: '',
+    header: 'header text',
+    footer: 'footer text',
+    font: 'Courier',
+    color: '#008000',
+    size: '24px',
+    imageURL: 'https://imgflip.com/s/meme/Creepy-Condescending-Wonka.jpg',
     image: ''
   }
 
   onChange = ({ target }) => {
-    this.setState( { [target.name]: target.value })
+    this.setState({ [target.name]: target.value })
   }
+
+  onImageUpload = ({target}) => {
+    this.setState({ imageURL: window.URL.createObjectURL(target.files[0]) })
+  }
+
+  textToImage = (event) => {
+    event.preventDefault();
+    domToImage.toPng(document.getElementById('imageContainer'))
+      .then(img => {
+        this.setState({ image });
+      });
+  };
+
+  saveImage = () => {
+    fileSaver.saveAs(this.state.image);
+  };
 
 render () {
 
-  const { header, footer, font, color, image } = this.state;
+  const { header, footer, font, color, imageURL, image } = this.state;
+
+  const imageContainerStyle = {
+    position: 'relative',
+    fontFamily: font,
+    color: color
+  }
+  const headerStyle = {
+    position: 'absolute',
+    top: '15px',
+    left: '15px'
+  }
+  const footerStyle = {
+    position: 'absolute',
+    bottom: '15px',
+    left: '15px'
+  }
+
   const fontOptions = ['Times', 'Courier', 'Arial'].map(font => {
     return <option key={font} value={font}>{font}</option>
   });
@@ -27,24 +63,43 @@ render () {
   return (
     <Fragment>
       <h1>Meme Generator</h1>
-      <label htmlFor="header">Header: </label>
-      <input name="header" placeholder="enter header here" value={header} onChange={this.onChange} />
-      <p>{header}</p>
-      <label htmlFor="footer">Footer: </label>
-      <input name="footer" placeholder="enter footer here" value={footer} onChange={this.onChange} />
-      <p>{footer}</p>
-      <label htmlFor="font">Font family: </label>
-      <select name="font" defaultValue={font} onChange={this.onChange}>
-        {fontOptions}
-      </select>
-      <p>{font}</p>
-      <label htmlFor="color">Font color: </label>
-      <input name="color" value={color} type="color" onChange={this.onChange} />
-      <p>{color}</p>
-      <label htmlFor="image"></label>
-      <input name="image" type="file" accept="image/*" onChange={this.onChange} />
-      {/* {image && <img src={image} />} */}
-      <p>{image}</p>
+
+      <form onSubmit={this.textToImage}>
+        <fieldset>
+          <legend>Options</legend>
+          <label htmlFor="header">Header: </label>
+          <input name="header" placeholder="enter header here" value={header} onChange={this.onChange} />
+
+
+          <label htmlFor="footer">Footer: </label>
+          <input name="footer" placeholder="enter footer here" value={footer} onChange={this.onChange} />
+
+          <label htmlFor="font">Font family: </label>
+          <select name="font" defaultValue={font} onChange={this.onChange}>
+            {fontOptions}
+          </select>
+          <p>{font}</p>
+
+          <label htmlFor="color">Font color: </label>
+          <input name="color" value={color} type="color" onChange={this.onChange} />
+          <p>{color}</p>
+
+          <label htmlFor="imageURL">Image URL</label>
+          <input name="imageURL" type="text" onChange={this.onChange} /><br />
+
+          <label htmlFor="imageUpload"></label>
+          <input name="imageUpload" type="file" accept="image/*" onChange={this.onImageUpload} />
+          <p>{imageURL}</p>
+        </fieldset>
+      </form>
+
+      <div id="imageContainer" style={imageContainerStyle}>
+        {imageURL && <img src={imageURL} />}
+        <div style={headerStyle}>{header}</div>
+        <div style={footerStyle}>{footer}</div>
+      </div>
+      {image && <img src={image} />}
+      {imageURL && <button onClick={this.saveImage}>Save Image</button>}
     </Fragment>
   )
 
