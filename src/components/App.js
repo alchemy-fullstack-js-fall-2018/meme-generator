6d117a9b204557908a899b429ec60bf5989d2190
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-13 16:39:19 -0800

    form created

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, { Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class App extends Component {
 
   render() {
     return (
       <Fragment>
-        <h1>Hello World</h1>
+        <h1>Lets Make Some Memes!</h1>
+        <form>
+          <input placeholder="Add an Image"/>
+          <input placeholder="Add Header"/>
+          <input placeholder="Add Footer"/>
+          <button type="submit">Make a Meme</button>
+        </form>
       </Fragment>
     )
   }
