0d51bb8c168b91ccfddb37b0099fa46b0b3a73e3
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-14 09:50:39 -0800

    able to upload file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component, Fragment } from 'react';
-
+import styles from './App.css';
 export default class App extends Component {
 
   state = {
-    url: '',
+    url: 'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
     header: '',
     footer: '',
+  }
+
+
+
+  handleChosenFile = ({ target }) => {
+    let url = window.URL.createObjectURL(target.files[0])
+    this.setState({ url })
   }
 
   onChange = ({ target }) => {
@@ -18,13 +25,18 @@ export default class App extends Component {
       <Fragment>
         <h1>Lets Make Some Memes!</h1>
         <form>
-          <input name="url" placeholder="Add an Image URL" value={url} onChange={this.onChange}/>
-          <input name="header" placeholder="Add Header" value={header} onChange={this.onChange}/>
-          <input name="footer" placeholder="Add Footer" value={footer} onChange={this.onChange}/>
-          <button type="submit">Make a Meme</button>
+          <input name="url" placeholder="Add an Image URL" value={url} onChange={this.onChange}/><br/>
+          <input name="file" type="file" onChange={this.handleChosenFile}/><br/>
+          <input name="header" placeholder="Add Header" value={header} onChange={this.onChange}/><br/>
+          <input name="footer" placeholder="Add Footer" value={footer} onChange={this.onChange}/><br/>
+          <button type="submit">Save This Meme</button>
         </form>
-        <div>
-          <img src={url} alt="meme"/>
+        <div id="meme" className={styles.meme}>
+          <img id="img"src={url} alt="meme" className={styles.image}/>
+          <div className={styles.text}>
+            <span className={styles.header}>{header}</span>
+            <span className={styles.footer}>{footer}</span>
+          </div>
         </div>
       </Fragment>
     )
