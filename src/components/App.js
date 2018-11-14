55fb0edc03654862d041c94b893981a0197a3bc7
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-13 17:33:53 -0800

    image showing on url link

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,30 @@ import React, { Component, Fragment } from 'react';
 
 export default class App extends Component {
 
+  state = {
+    url: '',
+    header: '',
+    footer: '',
+  }
+
+  onChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
+    const { url, header, footer } = this.state;
     return (
       <Fragment>
         <h1>Lets Make Some Memes!</h1>
         <form>
-          <input placeholder="Add an Image"/>
-          <input placeholder="Add Header"/>
-          <input placeholder="Add Footer"/>
+          <input name="url" placeholder="Add an Image URL" value={url} onChange={this.onChange}/>
+          <input name="header" placeholder="Add Header" value={header} onChange={this.onChange}/>
+          <input name="footer" placeholder="Add Footer" value={footer} onChange={this.onChange}/>
           <button type="submit">Make a Meme</button>
         </form>
+        <div>
+          <img src={url} alt="meme"/>
+        </div>
       </Fragment>
     )
   }
