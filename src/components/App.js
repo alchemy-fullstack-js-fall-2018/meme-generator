2412dfb117a52a4d5a017a333d98e20e038b379b
mlamb <mlamb128@gmail.com>
2018-11-13 16:11:29 -0800

    Basic scaffolding complete and App is compiling and rendering to browser

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import domToImage from 'dom-to-image';
+import fileSaver from 'file-saver';
+import styles from './App.css';
+
+export default class App extends Component{
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+      </Fragment>
+    );
+  }
+}
