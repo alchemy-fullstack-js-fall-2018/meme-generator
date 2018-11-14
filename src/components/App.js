383a442d8806613e001a3cdca6d0c95de13a2fa0
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-13 17:19:08 -0800

    1st form element rendering to the UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component, Fragment } from 'react';
 export default class App extends Component {
-    state = {};
+    state = {
+        imgSource: 'URL' 
+    };
 
     render() {
+        const { imgSource } = this.state;
+        const options = ['URL', 'Upload'].map(option => {
+            return <option key={option} value={option}>{option}</option>;
+        });
+
         return (
             <Fragment>
                 <h1>Hello Meme Generator</h1>
+                <form>
+                    <select name="imgSource" defaultValue={imgSource}>
+                        {options}
+                    </select>
+                </form>
             </Fragment>
         );
     }
