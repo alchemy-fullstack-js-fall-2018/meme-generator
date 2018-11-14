896c518835a4c52672d125a5991e8d98414b81e2
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-14 14:47:03 -0800

    all basic functionality present except fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import styles from './App.css';
+import fileSaver from 'file-saver';
+import domToImage from 'dom-to-image';
 export default class App extends Component {
     state = {
         imgSource: 'URL',
@@ -7,7 +9,9 @@ export default class App extends Component {
         topText: '',
         bottomText: '',
         topColor: 'white',
-        bottomColor: 'white'
+        bottomColor: 'white',
+        memeImg: '',
+        font: ''
     };
 
     onChange = ({ target }) => {
@@ -15,14 +19,23 @@ export default class App extends Component {
     };
 
     onUpload =  ({ target }) => {
-        console.log('files[0]', target.files[0]);
         let imgURL = window.URL.createObjectURL(target.files[0]);
-        console.log('imgURL', imgURL);
         this.setState({ img: imgURL });
     };
 
+    memeToImage = event => {
+        event.preventDefault();
+        domToImage.toPng(document.getElementById('meme')).then(memeImg => {
+            this.setState({ memeImg });
+        });
+    };
+
+    saveImage = () => {
+        fileSaver.saveAs(this.state.memeImg);
+    };
+
     render() {
-        const { imgSource, img, topText, bottomText, topColor, bottomColor } = this.state;
+        const { imgSource, img, topText, bottomText, topColor, bottomColor, font } = this.state;
         const options = ['URL', 'Upload'].map(imgSource => {
             return <option key={imgSource} value={imgSource}>{imgSource}</option>;
         });
@@ -31,6 +44,9 @@ export default class App extends Component {
         });
         const bottomColorOptions = ['black', 'white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'grey'].map(bottomColor => {
             return <option key={bottomColor} value={bottomColor}>{bottomColor}</option>;
+        });
+        const fontOptions = ['Arial', 'Times New Roman', 'Garamond', 'Comic Sans'].map(font => {
+            return <option key={font} value={font}>{font}</option>;
         });
         let input = <input type="text" name="img" value={img} onChange={this.onChange}/>;
         if(imgSource === 'Upload') {
@@ -42,24 +58,29 @@ export default class App extends Component {
         return (
             <Fragment>
                 <h1>Hello Meme Generator</h1>
-                <form>
+                <form onSubmit={this.memeToImage}>
                     <select name="imgSource" defaultValue={imgSource} onChange={this.onChange}>
                         {options}
                     </select>
                     {input}
+                    <select name="font" defaultValue={font} onChange={this.onChange}>
+                        {fontOptions}
+                    </select>
                     <select name="topColor" defaultValue={topColor} onChange={this.onChange}>
                         {topColorOptions}
                     </select>
+                    {topTextInput}
                     <select name="bottomColor" defaultValue={bottomColor} onChange={this.onChange}>
                         {bottomColorOptions}
                     </select>
-                    {topTextInput}
                     {bottomTextInput}
+                    <button>Create Image</button>
                 </form>
-                {img && <div className={styles.card} style={{ backgroundImage: `url(${img})` }}>
-                    <p className={styles.topText} style={{ color: topColor }}>{topText}</p>
+                <div className={styles.card} style={ { fontFamily: font } } id="meme">                    {img && <img src={img}/>}
+                    <p className={styles.topText} style={ { color: topColor } }>{topText}</p>
                     <p className={styles.bottomText} style={{ color: bottomColor }}>{bottomText}</p>
-                </div>}
+                </div>
+                <button onClick={this.saveImage}>Save Image</button>
             </Fragment>
         );
     }
