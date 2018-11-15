7f762fab217d8661e2adf241d2d9bc3619a4667d
Davidchhing <davidhchhing@gmail.com>
2018-11-14 16:16:07 -0800

    styled form and added text to body of img

--- src/components/App.js
+++ src/components/App.js
@@ -9,28 +9,20 @@ const colorsOptions = require('colors');
 export default class App extends Component {
   state = {
     font: 'Standard',
-    text: '',
-    formatedText: '',
+    textArea: '',
     img: 'http://i.imgflip.com/2mk6g6.jpg',
     color: ''
   };
 
   onChange = ({ target }) => {
     // setState in async. It waits for all components to update
-    this.setState({ [target.name]: target.value, img: '' }, () => {
-      const { text, font, color, img } = this.state;
-      figlet.text(text, { font, color, img }, (err, formatedText) => {
-        this.setState({ formatedText });
-      });
-    });
+    this.setState({ [target.name]: target.value });
   };
 
   saveMeme = (event) => {
     event.preventDefault();
-    domToImage.toPng(document.getElementById('memeImage'))
-      .then(memeImage => {
-        this.setState({ memeImage });
-      });
+    domToImage.toPng(document.getElementById('meme'))
+      .then(memeImage => fileSaver.saveAs(memeImage));
   };
 
   saveImage = () => {
@@ -38,52 +30,59 @@ export default class App extends Component {
   };
 
   render() {
-    const { font, text, formatedText, img, color  } = this.state;
+    const { font, textArea, img, color  } = this.state;
     const fontOptions = ['Standard', 'Bell', '3-D', 'Avatar', 'Barbwire'].map(font => {
       return <option key={font} value={font}>{font}</option>;
     });
 
     const memeTextStyles = () => {
       return {
-        color: color
+        color: color,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
       }
     }
 
 
     return (
+
       <Fragment>
+      <h1>Meme Generator</h1>
+
         <form onSubmit={this.saveMeme}>
-          <div id="images">
-            <label htmlFor="img">Enter image URL or upload image</label>
+          <div className={styles.labelContainer}>
+            <label htmlFor="img">Enter URL</label>
             <input type="text" name="img" id="url" placeholder="Enter Image URL" value={img} onChange={this.onChange} />
           </div>
+          <div className={styles.labelContainer}>
+            <label htmlFor="font">Select Font</label>
+            <select name="font" defaultValue={font} onChange={this.onChange}>
+              {fontOptions}
+            </select>
+          </div>
+          <div className={styles.labelContainer}>
+            <label htmlFor="color">Choose Text Color:</label>
+            <input name="color" type="color" value={color} onChange={this.onChange}></input>
+          </div>
+          <div className={styles.labelContainer}>
+            <label htmlFor="textArea">Enter Text here</label>
+            <input name="textArea" type="text" value={textArea} onChange={this.onChange} />
+          </div>
+          <div className={styles.labelContainer}>
+            <button type="submit">Save meme</button>
+          </div>
+        </form>
 
-          <label htmlFor="font">Select Font</label>
-          <select name="font" defaultValue={font} onChange={this.onChange}>
-            {fontOptions}
-          </select>
-
-          <label htmlFor="color">Choose Text Color:</label>
-          <input
-            name="color" type="color" value={color} onChange={this.onChange}>
-          </input>
-
-          <input name="text" placeholder="put text here" value={text} onChange={this.onChange} />
-          <button type="submit">Save meme</button>
-          </form>
-
-
-        <div className={styles.image}>
-          <span id="memeImage" >
-            {formatedText}
-          </span>
+        <div id="meme" className={styles.meme}>
+          <img className={styles.imageMeme} src={img} />
+          <div className={styles.memeText}>
+            <p style={memeTextStyles()}>
+              {textArea}
+            </p>
+          </div>
         </div>
-        {img && <img src={img} />}
-        {img && <button onClick={this.saveImage}>Save Image</button>}
-
-
-
-
 
       </Fragment>
     );
